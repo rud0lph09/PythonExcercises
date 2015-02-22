f12a48a0f6069a0bae4f82ee1532f36581068539
Rodofo Castillo <rovidrio@comunidad.unam.mx>
2015-02-22 16:40:31 -0600

    Capitulo 4
    Ejercicios 1 y 2 (recordar que la cuenta de ejercicios es según mi
    cuenta y no tiene nada que ver con la cuenta de ejercicios del libro)
    recomendable leer el capitulo completo antes de consultar mis ejercicios

---
+++ c4e2.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*- 
+"""	Rodolfo Castillo Vidrio. 2015. 			Todos los ejercicios desarrollados en este repo se basan en el libro "think python" por Allen Downey.
+											Every excercise developed in this repo is based on the book "think python" by Allen Downey.
+
+Español : 	Los ejercicios en este repo, no estaran numerados en base al libro, si no conforme los temas que se vean en cada capítulo
+			c# indica el capitulo, e# indica el numero de ejercicio segun MI CUENTA .
+
+English :	This excercises are not named after the excercices of the book but instead are named after the chapters C# marks the chapter
+			while e# marks the number of the excercise wich is based entirely on MY OWN COUNT.
+			This are scripts to be excecuted by the python translator.
+			Definitions and comentaries will be in Spanish. Some terms will be on English. Take this into account if you wish to use my excercises as guides
+			The reason to this is because this repo is ment to help students from my College
+
+
+Instalando Swampy: 	Swampy es una suite creada por Allen Downey, la cual sirve de entorno gráfico y sera necesaria apartir del 4o Capitulo de su libro
+					Para revisar si se tiene swampy, ejecutar el interprete de python en consola con el comando "python", posteriormente escribir:
+
+						import swampy.TurtleWorld
+
+					Si no obtienes ningun mensaje de error puedes entonces continuar. De lo contrario es necesario instalar. Ejecuta el siguiente comando
+					en consola:
+
+						install pip swampy   		o 			sudo install pip swampy
+
+					Una vez terminado puedes continuar con el capitulo 4
+
+Installing Swampy: 	Please go to: http://thinkpython.om/swampy for instructions on installing swampy
+
+
+"""
+
+#Propuesta: Crear una tortuga en swampy y luego enviarla como argumento a una funcion que cree un cuadrado
+
+from swampy.TurtleWorld import *			#Importamos el modulo TurtleWorld de swampy
+
+miMundo = TurtleWorld()						#creamos un objeto llamado miMundo de tipo TurtleWorld
+monzi = Turtle()							#creamos una tortuga con nombre monzi
+print monzi									#la ponemos en pantalla
+
+def dibujaSqr(tortuga):						#definimos la funcion para dibujar un cuadrado
+	for i in range(4):						#ciclo for para repetir un loop 4 veces
+		bk(tortuga, 100); rt(tortuga)		#usamos funcion atras y derecha para ir moviendo a la tortuga
+
+dibujaSqr(monzi)							#llamamos la funcion para poder dibujar el cuadrado
+
+print 										#simples print para dejar espacio e imprimir una instruccion al usuario
+print
+print "El proceso ha terminado, puedes cerrar la ventana de TurtleWorld cuando desees para terminar el programa."
+print
+
+wait_for_user()								#esperamos a que el usuario cierre la ventana
+
+print "Hasta luego! :)"						#mensaje de despedida
+print
+
+
+"""
+Español: Para ejecutar solo ir a la carpeta (desde la terminal) y ejecutar el comando "python nombre.py" por nombre me refiero al nombre del ejercicio
+
+English: To execute just enter the folder you saved this file (from the terminal) and execute the command "python name.py"
+"""
