ae98c8a968b032114b84e4672248a2ef795c77e0
Rodofo Castillo <rovidrio@comunidad.unam.mx>
2015-02-22 02:10:28 -0600

    Capitulo 3
    Ejercicio 1 Diseñado en base al tema de funciones

---
+++ c3e1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*- 
+"""	Rodolfo Castillo Vidrio. 2015. 			Todos los ejercicios desarrollados en este repo se basan en el libro "think python" por Allen Downey.
+											Every excercise developed in this repo is based on the book "think python" by Allen Downey.
+
+Español : 	Los ejercicios en este repo, no estaran numerados en base al libro, si no conforme los temas que se vean en cada capítulo
+			c# indica el capitulo, e# indica el numero de ejercicio segun MI CUENTA .
+
+English :	This excercises are not named after the excercices of the book but instead are named after the chapters C# marks the chapter
+			while e# marks the number of the excercise wich is based entirely on MY OWN COUNT.
+			This are scripts to be excecuted by the python translator.
+			Definitions and comentaries will be in Spanish. Some terms will be on English. Take this into account if you wish to use my excercises as guides
+			The reason to this is because this repo is ment to help students from my College
+"""
+
+#En este ejercicio hacemos una muestra de una funcion
+
+def calAerea(b, h):    	#definimos la funcion y que le podemos enviar dos variables como argumento
+	area = b * h 		#hacemos el calculo del area
+	return area 		#regresamos el valor del area
+
+print calAerea(2, 2) 	#imprimos el valor de regreso cuando la base mide 2 y la altura mide dos
+
+"""
+Español: Para ejecutar solo ir a la carpeta (desde la terminal) y ejecutar el comando "python nombre.py" por nombre me refiero al nombre del ejercicio
+
+English: To execute just enter the folder you saved this file (from the terminal) and execute the command "python name.py"
+"""
---
+++ c3e2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+"""	Rodolfo Castillo Vidrio. 2015. 			Todos los ejercicios desarrollados en este repo se basan en el libro "think python" por Allen Downey.
+											Every excercise developed in this repo is based on the book "think python" by Allen Downey.
+
+Español : 	Los ejercicios en este repo, no estaran numerados en base al libro, si no conforme los temas que se vean en cada capítulo
+			c# indica el capitulo, e# indica el numero de ejercicio segun MI CUENTA .
+
+English :	This excercises are not named after the excercices of the book but instead are named after the chapters C# marks the chapter
+			while e# marks the number of the excercise wich is based entirely on MY OWN COUNT.
+			This are scripts to be excecuted by the python translator.
+			Definitions and comentaries will be in Spanish. Some terms will be on English. Take this into account if you wish to use my excercises as guides
+			The reason to this is because this repo is ment to help students from my College
+"""
+
+
+
+"""
+Español: Para ejecutar solo ir a la carpeta (desde la terminal) y ejecutar el comando "python nombre.py" por nombre me refiero al nombre del ejercicio
+
+English: To execute just enter the folder you saved this file (from the terminal) and execute the command "python name.py"
+"""
